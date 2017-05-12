622901bf173ba4f691256ecb75cb7cfca99fa274
Priya Jain <priya.j@planetwebsolution.com>
2017-05-12 19:23:49 +0530

    i added head.php file

---
+++ includes/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="assets/bootstrap/datetimepicker/css/bootstrap-datetimepicker.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/plugins/animate/css/animate.css">
+<link rel="stylesheet" type="text/css" href="assets/my-fonts/style.css">
+
+<link rel="stylesheet" href="assets/plugins/owl.carousel.2.2.0/assets/assets/owl.carousel.min.css">
+<!--<link rel="stylesheet" href="assets/plugins/owl.carousel.2.2.0/assets/owl.theme.default.min.css">-->
+
+<link href="assets/css/cssmin.css" rel="stylesheet" type="text/css" />
+
+
