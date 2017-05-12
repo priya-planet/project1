6acc8dfab7a4d009fca015b679443c96b3982ac9
Priya Jain <priya.j@planetwebsolution.com>
2017-05-12 18:06:03 +0530

    index.php file is added

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include("includes/head.php"); ?>
+<!--head end -->
+<title>Urban Staff</title>
+</head>
+
+<body>
+
+<!--header start -->
+<?php include("includes/header.php"); ?>
+<!--header end --> 
+
+
+<!--footer start -->
+<?php include("includes/footer.php"); ?>
+<!--footer end -->
+
+<!--footer start -->
+<?php include("includes/script.php"); ?>
+<!--footer end -->
+
+</body>
+</html>
+<!--body end-->
