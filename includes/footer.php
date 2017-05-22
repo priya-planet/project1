3e7a80bd6ddf21007eecf7eccd97c5008ab975bd
Priya Jain <priya.j@planetwebsolution.com>
2017-05-22 17:44:08 +0530

    container with new footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,49 +1,100 @@
-
-
-<!--footer start-->
 <footer>
+<section class="nb-footer">
+<div class="container">
+<div class="row">
+<div class="col-md-3 col-sm-6">
+	<div class="footer-single">
+	<!-- 	<img src="images/logo.png" class="img-responsive" alt="Logo"> -->
 
+		<!-- This is only for better view of theme if you want your image logo remove div dummy-logo bellow and replace your logo in logo.png and uncomment logo tag above-->
+	<div class="dummy-logo">
+	<div class="icon pull-left brand">
+		<i class="fa fa-copy"></i>
+	</div>
+		<h2>Next Boostrap</h2>
+		<p>Another responsive footer</p>
+	</div>
 
-<div id="footer">
-    <div class="container">
-        <div class="row">
-            <h3 class="footertext">About Us:</h3>
-            <br>
-              <div class="col-md-4">
-                <center>
-                  <img src="http://oi60.tinypic.com/w8lycl.jpg" class="img-circle" alt="the-brains">
-                  <br>
-                  <h4 class="footertext">Programmer</h4>
-                  <p class="footertext">You can thank all the crazy programming here to this guy.<br>
-                </center>
-              </div>
-              <div class="col-md-4">
-                <center>
-                  <img src="http://oi60.tinypic.com/2z7enpc.jpg" class="img-circle" alt="...">
-                  <br>
-                  <h4 class="footertext">Artist</h4>
-                  <p class="footertext">All the images here are hand drawn by this man.<br>
-                </center>
-              </div>
-              <div class="col-md-4">
-                <center>
-                  <img src="http://oi61.tinypic.com/307n6ux.jpg" class="img-circle" alt="...">
-                  <br>
-                  <h4 class="footertext">Designer</h4>
-                  <p class="footertext">This pretty site and the copy it holds are all thanks to this guy.<br>
-                </center>
-              </div>
-            </div>
-            <div class="row">
-            <p><center><a href="#">Contact Stuff Here</a> <p class="footertext">Copyright 2014</p></center></p>
-        </div>
-    </div>
+		<p>Next Bootstrap is ultimate resource for bootstrap. You can find free themes, psd templates, code snippets, adons and tips updated regularly. You can send your creative design to us.</p>
+		<a href="" class="btn btn-footer">Read More</a>
+	</div>
 </div>
 
+<div class="col-md-3 col-sm-6">
+	<div class="footer-single useful-links">
+	 <div class="footer-title"><h2>Navigation</h2></div>
+	 <ul class="list-unstyled">
+            <li><a href="index.html">Home <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="about1.html">About Us <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="service1.html">Services <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="portfolio1.html">Portfolio <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="pricing.html">Pricing <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="contact1.html">Contact Us <i class="fa fa-angle-right pull-right"></i></a></li>
+        </div>
+</div>
+<div class="clearfix visible-sm"></div>
+
+<div class="col-md-3 col-sm-6">
+	
+	<div class="col-sm-12 left-clear right-clear footer-single footer-project">
+		<div class="footer-title"><h2>Latest Projects</h2></div>
+		<div class="col-xs-4">
+			<img src="images/projects/project1.jpg" class="img-responsive center-block">
+		</div>
+		<div class="col-xs-4">
+			<img src="images/projects/project2.jpg" class="img-responsive center-block">
+		</div>
+		<div class="col-xs-4">
+			<img src="images/projects/project3.jpg" class="img-responsive center-block">
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-xs-4">
+			<img src="images/projects/project4.jpg" class="img-responsive center-block">
+		</div>
+		<div class="col-xs-4">
+			<img src="images/projects/project5.jpg" class="img-responsive center-block">
+		</div>
+		<div class="col-xs-4">
+			<img src="images/projects/project6.jpg" class="img-responsive center-block">
+		</div>
+
+	</div>
+
+</div>
+
+<div class="col-md-3 col-sm-6">
+	<div class="footer-single">
+		<div class="footer-title"><h2>Contact Information</h2></div>
+		<address>
+			999, Holly Eiusmod Tempor, City<br>
+			Your State, Country <br>
+			<i class="fa fa-phone"></i>  987 654 3210 <br>
+			<i class="fa fa-fax"></i> 012 123 2345<br>
+			<i class="fa fa-envelope"></i> info@example.com<br>
+		</address>					
+	</div>
+</div>
+
+</div>
+</div>
+</section>	
+
+<section class="nb-copyright">
+<div class="container">
+<div class="row">
+<div class="col-sm-6 copyrt xs-center">
+	2015 &copy; All Rights Reserved. <a href="">Terms & Conditions</a> | <a href="">Privacy Policy</a>
+</div>
+<div class="col-sm-6 text-right xs-center">
+	<ul class="list-inline footer-social">
+		<li><a href=""><i class="fa fa-facebook"></i></a></li>
+		<li><a href=""><i class="fa fa-twitter"></i></a></li>
+		<li><a href=""><i class="fa fa-youtube-play"></i></a></li>
+		<li><a href=""><i class="fa fa-google-plus"></i></a></li>
+		<li><a href=""><i class="fa fa-skype"></i></a></li>
+	</ul>
+</div>
+</div>
+</div>
+</section>
 </footer>
-<!--footer end-->
-
-
-
-
-
--- index.php
+++ index.php
@@ -9,7 +9,30 @@
 <?php include("includes/header.php"); ?>
 <!--header end --> 
 
+<div class="page-container"><div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <i style="color:#29abe0;font-size:4em;" class="fa fa-server" aria-hidden="true"></i><br><br>
+                <strong style="font-size:18px;">Lorem Ipsum Dolor</strong>
+                <br>
+                <p style="font-size:13px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.</p>
+            </div>
 
+            <div class="col-sm-4">
+                <i style="color:#29abe0;font-size:4em;" class="fa fa-cloud-upload" aria-hidden="true"></i><br><br>
+                <strong style="font-size:18px;">Cras Id Augue Vel Lacus Egestas</strong>
+                <br>
+                <p style="font-size:13px;">Nulla eget sem nec nibh volutpat imperdiet ut iaculis eros. Suspendisse potenti. Vestibulum euismod dolor id quam vulputate rutrum. Phasellus in turpis in erat iaculis molestie. Cras sem nisl, vestibulum sed mi sit amet, luctus molestie libero. Vestibulum libero odio. Tapeo dimo nu lu crasas mala.</p>
+            </div>
+
+            <div class="col-sm-4">
+                <i style="color:#29abe0;font-size:4em;" class="fa fa-file-image-o" aria-hidden="true"></i><br><br>
+                <strong style="font-size:18px;">Nulla Eget Sem Nec Nibh </strong>
+                <br>
+                <p style="font-size:13px;">Cras id augue vel lacus egestas convallis. Proin hendrerit enim at purus vehicula, ac mattis sapien auctor. In congue eros est, vitae ultricies dui cursus ut. In justo nunc, rutrum nec lacinia auctor, maximus et velit. In hac habitasse platea dictumst, vestibulum tincidunt maximus et, placerat et lectus.</p>
+            </div>
+        </div>
+    </div></div>
 <!--footer start -->
 <?php include("includes/footer.php"); ?>
 <!--footer end -->
