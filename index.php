5113f182bb0b2503dce6e3c5df51b99f8353ed26
Priya Jain <priya.j@planetwebsolution.com>
2017-05-23 16:38:39 +0530

    full banner added

--- index.php
+++ index.php
@@ -7,99 +7,86 @@
 
 <!--header start -->
 <?php include("includes/header.php"); ?>
-<!--header end --> 
+<!--header end -->
 
-<div class="page-container"><div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <i style="color:#29abe0;font-size:4em;" class="fa fa-server" aria-hidden="true"></i><br><br>
-                <strong style="font-size:18px;">Lorem Ipsum Dolor</strong>
-                <br>
-                <p style="font-size:13px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.</p>
-            </div>
-
-            <div class="col-sm-4">
-                <i style="color:#29abe0;font-size:4em;" class="fa fa-cloud-upload" aria-hidden="true"></i><br><br>
-                <strong style="font-size:18px;">Cras Id Augue Vel Lacus Egestas</strong>
-                <br>
-                <p style="font-size:13px;">Nulla eget sem nec nibh volutpat imperdiet ut iaculis eros. Suspendisse potenti. Vestibulum euismod dolor id quam vulputate rutrum. Phasellus in turpis in erat iaculis molestie. Cras sem nisl, vestibulum sed mi sit amet, luctus molestie libero. Vestibulum libero odio. Tapeo dimo nu lu crasas mala.</p>
-            </div>
-
-            <div class="col-sm-4">
-                <i style="color:#29abe0;font-size:4em;" class="fa fa-file-image-o" aria-hidden="true"></i><br><br>
-                <strong style="font-size:18px;">Nulla Eget Sem Nec Nibh </strong>
-                <br>
-                <p style="font-size:13px;">Cras id augue vel lacus egestas convallis. Proin hendrerit enim at purus vehicula, ac mattis sapien auctor. In congue eros est, vitae ultricies dui cursus ut. In justo nunc, rutrum nec lacinia auctor, maximus et velit. In hac habitasse platea dictumst, vestibulum tincidunt maximus et, placerat et lectus.</p>
-            </div>
-        </div>
-        
-        
-        
+<div class="page-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4"> <i style="color:#29abe0;font-size:4em;" class="fa fa-server" aria-hidden="true"></i><br>
+        <br>
+        <strong style="font-size:18px;">Lorem Ipsum Dolor</strong> <br>
+        <p style="font-size:13px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.</p>
+      </div>
+      <div class="col-sm-4"> <i style="color:#29abe0;font-size:4em;" class="fa fa-cloud-upload" aria-hidden="true"></i><br>
+        <br>
+        <strong style="font-size:18px;">Cras Id Augue Vel Lacus Egestas</strong> <br>
+        <p style="font-size:13px;">Nulla eget sem nec nibh volutpat imperdiet ut iaculis eros. Suspendisse potenti. Vestibulum euismod dolor id quam vulputate rutrum. Phasellus in turpis in erat iaculis molestie. Cras sem nisl, vestibulum sed mi sit amet, luctus molestie libero. Vestibulum libero odio. Tapeo dimo nu lu crasas mala.</p>
+      </div>
+      <div class="col-sm-4"> <i style="color:#29abe0;font-size:4em;" class="fa fa-file-image-o" aria-hidden="true"></i><br>
+        <br>
+        <strong style="font-size:18px;">Nulla Eget Sem Nec Nibh </strong> <br>
+        <p style="font-size:13px;">Cras id augue vel lacus egestas convallis. Proin hendrerit enim at purus vehicula, ac mattis sapien auctor. In congue eros est, vitae ultricies dui cursus ut. In justo nunc, rutrum nec lacinia auctor, maximus et velit. In hac habitasse platea dictumst, vestibulum tincidunt maximus et, placerat et lectus.</p>
+      </div>
     </div>
-    
+  </div>
+  <br>
+  <br>
+  <div class="container">
+    <div class="row thumb-row">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
+        <!-- Recommended size 360X360 --> 
+        <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
+        <div class="quotes">
+          <h1>Lorem ipsum dolor</h1>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
+        </div>
+        </a> </div>
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
+        <!-- Recommended size 360X360 --> 
+        <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
+        <div class="quotes">
+          <h1>Lorem ipsum dolor</h1>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
+        </div>
+        </a> </div>
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
+        <!-- Recommended size 360X360 --> 
+        <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
+        <div class="quotes">
+          <h1>Lorem ipsum dolor</h1>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
+        </div>
+        </a> </div>
+    </div>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
     <br>
-<br>
-
-
-    
-    <div class="container"><div class="row thumb-row">
-<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-            <a class="list-quotes" href="/">
-                <!-- Recommended size 360X360 -->
-                <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
-                <div class="quotes">
-                    <h1>Lorem ipsum dolor</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span>
-                    </p>
-                </div>
-            </a>
-        </div><div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-            <a class="list-quotes" href="/">
-                <!-- Recommended size 360X360 -->
-                <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
-                <div class="quotes">
-                    <h1>Lorem ipsum dolor</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span>
-                    </p>
-                </div>
-            </a>
-        </div><div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-            <a class="list-quotes" href="/">
-                <!-- Recommended size 360X360 -->
-                <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
-                <div class="quotes">
-                    <h1>Lorem ipsum dolor</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span>
-                    </p>
-                </div>
-            </a>
-        </div>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
+    <br>
+  </div>
 </div>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br>
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br>
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br>
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br></div>
-    
+<div class="image-container">
+ 
+    <div class="text">
+        <h1>header text</h1>
+            <span>price</span>
+            <span>Location</span>
+            <span>days</span>    
     </div>
+</div>
+    
 
 
 
 
 
-  
 <!--footer start -->
 <?php include("includes/footer.php"); ?>
-<!--footer end -->
+<!--footer end --> 
 
 <!--footer start -->
 <?php include("includes/script.php"); ?>
