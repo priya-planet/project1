2693071195ebe66ec275bac5a7cce2eaaf7b6ce9
Priya Jain <priya.j@planetwebsolution.com>
2017-05-24 14:23:56 +0530

    clear content code

--- index.php
+++ index.php
@@ -28,83 +28,41 @@
         <p style="font-size:13px;">Cras id augue vel lacus egestas convallis. Proin hendrerit enim at purus vehicula, ac mattis sapien auctor. In congue eros est, vitae ultricies dui cursus ut. In justo nunc, rutrum nec lacinia auctor, maximus et velit. In hac habitasse platea dictumst, vestibulum tincidunt maximus et, placerat et lectus.</p>
       </div>
     </div>
-  </div>
-  <br>
-  <br>
-  <div class="container">
-    <div class="row thumb-row">
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
-        <!-- Recommended size 360X360 --> 
-        <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
-        <div class="quotes">
-          <h1>Lorem ipsum dolor</h1>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
-        </div>
-        </a> </div>
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
-        <!-- Recommended size 360X360 --> 
-        <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
-        <div class="quotes">
-          <h1>Lorem ipsum dolor</h1>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
-        </div>
-        </a> </div>
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
-        <!-- Recommended size 360X360 --> 
-        <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
-        <div class="quotes">
-          <h1>Lorem ipsum dolor</h1>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
-        </div>
-<<<<<<< HEAD
-        </a> </div>
-    </div>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-    <br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-    <br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-    <br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-    <br>
+    </a> </div>
+</div>
+<br>
+<br>
+<div class="container">
+  <div class="row thumb-row">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
+      <!-- Recommended size 360X360 --> 
+      <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
+      <div class="quotes">
+        <h1>Lorem ipsum dolor</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
+      </div>
+      </a> </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
+      <!-- Recommended size 360X360 --> 
+      <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
+      <div class="quotes">
+        <h1>Lorem ipsum dolor</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
+      </div>
+      </a> </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"> <a class="list-quotes" href="/"> 
+      <!-- Recommended size 360X360 --> 
+      <img class='img-responsive' alt="img" src="https://img.sheroes.in/img/uploads/article/high_res/Woman-Traveling-Alone-1-1000x500.jpg">
+      <div class="quotes">
+        <h1>Lorem ipsum dolor</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>...Read More</span> </p>
+      </div>
+      </a> </div>
   </div>
 </div>
 
-<div class="image-container">
- 
-    <div class="text">
-        <h1>header text</h1>
-            <span>price</span>
-            <span>Location</span>
-            <span>days</span>    
-    </div>
-</div>
-    
+<div class="light-section"> </div>
 
-    </div></div>
-=======
-    </div></div>
-<div class="light-section">
-
-
-
-</div>  
-
-<div class="container"><br>
-<br>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br>
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br>
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br>
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque felis, malesuada eget sapien sit amet, posuere pretium tortor. Donec eu faucibus quam, non placerat sem. Ut luctus ultricies sapien, ac semper risus tincidunt sit amet. Maecenas ornare blandit malesuada. Param nun so laten luctum.<br>
-<br></div>
-  
->>>>>>> parent of 16f308d... thumb added
 <!--footer start -->
 <?php include("includes/footer.php"); ?>
 <!--footer end --> 
